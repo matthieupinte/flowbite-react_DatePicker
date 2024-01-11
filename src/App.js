5c520e1fcf4d35a3522a57a1cab7650fe97a6097
Matthieu Pinte <matthieupinte@gmail.com>
2024-01-11 16:57:06 +0100

    feat: CustomSearchBox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,90 @@
+import React, { useState } from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, useHits, useSearchBox } from "react-instantsearch";
+import { format } from 'date-fns';
 import "twin.macro";
+
+const APP_ID = 'H5IE0TSH1A';
+const API_KEY = '8135c22ab9185ac5b903a6de3165dd79';
+const INDEX_NAME = 'mooncard_development_matthieu_pinte';
+
+const searchClient = algoliasearch(APP_ID, API_KEY);
+
+const Hit = ({ hit }) => (
+  <tr>
+    <td>{format(hit.date, 'P')}</td>
+    <td>{hit.merchant_name}</td>
+    <td>{hit.title}</td>
+    <td>{hit.user_name}</td>
+    <td>{hit.expense_category}</td>
+    <td>{hit.receipt}</td>
+    <td>{hit.amount}</td>
+  </tr>
+)
+
+const CustomHits = (props) => {
+  const { hits, sendEvent } = useHits(props);
+
+  return (
+    <>
+      {hits.map((hit) => <props.hitComponent hit={hit} key={hit.objectID} onClick={() => sendEvent('click', hit, 'Hit Clicked')} />)}
+    </>
+  )
+}
+
+const CustomSearchBox = (props) => {
+  const { query, refine, /* clear */ } = useSearchBox(props);
+  const [value, setValue] = useState(query);
+
+  const setQuery = (value) => {
+    setValue(value);
+    refine(value);
+  };
+
+  return (
+    <form action="" role="search" noValidate>
+      <input type="search" value={value} onChange={e => setQuery(e.target.value)} />
+    </form>
+  );
+}
+
+const Filters = () => (
+  <aside tw="absolute top-0 left-0 z-40 w-64 h-screen p-4">
+    <div>
+      <h2>Search</h2>
+      <CustomSearchBox />
+    </div>
+  </aside>
+)
 
 function App() {
   return (
-    <div>
+    <section>
       <h1 tw="text-center">React App with Twin</h1>
-    </div>
+      <div tw="relative p-4">
+        <InstantSearch searchClient={searchClient} indexName={INDEX_NAME}>
+          <Filters />
+          <div tw="ml-64">
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Supplier</th>
+                  <th>Description</th>
+                  <th>Collaborator</th>
+                  <th>Kind</th>
+                  <th>Justificatif</th>
+                  <th>Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                <CustomHits hitComponent={Hit} />
+              </tbody>
+            </table>
+          </div>
+        </InstantSearch>
+      </div>
+    </section>
   );
 }
 
--- src/styles.js
+++ src/styles.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import { createGlobalStyle } from 'styled-components'
-import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro'
+import tw, { theme, globalStyles } from 'twin.macro'
+
+// FIX: https://github.com/ben-rogerson/twin.macro/issues/773
+const filteredGlobalStyles = Object.fromEntries(
+  Object.entries(globalStyles).filter(
+    k => k[0] !== "button, [type='button'], [type='reset'], [type='submit']",
+  ),
+);
+
+const BaseStyles = createGlobalStyle(filteredGlobalStyles);
 
 const CustomStyles = createGlobalStyle`
   body {
     -webkit-tap-highlight-color: ${theme`colors.purple.500`};
-    ${tw`antialiased`}
+    ${tw`text-sm antialiased`}
+  }
+
+  table {
+    ${tw`border-collapse`}
+  }
+
+  th, td {
+    ${tw`border border-gray-400 px-4 py-2`}
+  }
+
+  button {
+    ${tw`bg-transparent border border-gray-400 px-4 py-2 hover:bg-gray-100`}
+  }
+
+  input[type="text"], input[type="search"], input[type="number"], input[type="date"], select, textarea {
+    ${tw`border border-gray-400 px-4 py-2`}
   }
 `
 
